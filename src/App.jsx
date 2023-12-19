6e58d9dfdf6fbfbe861f1668bf54303c8fb75584
Jignesh <jignesh.sharma.707@gmail.com>
2023-12-19 19:22:36 +0530

    added: ReviewCard Commited

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Collections from './components/Collections';
 import Features from './components/Features';
+import Footer from './components/Footer';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import Product from './components/Product';
+import Review from './components/Review';
 import Shop from './components/Shop';
 
 function App() {
@@ -24,6 +26,11 @@ function App() {
         <div id="products">
           <Product />
         </div>
+
+        <div id="review">
+          <Review />
+        </div>
+        <Footer />
       </div>
     </>
   );
